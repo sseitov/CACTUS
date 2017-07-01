d8a590508fa62e81ed58ffa1da34f61955d0904d
Сергей Сейтов <sseitov@imac.local>
2017-07-01 20:54:20 +0300

    fix

--- app/src/main/java/com/vchannel/cactus/MainActivity.java
+++ app/src/main/java/com/vchannel/cactus/MainActivity.java
@@ -2,7 +2,8 @@ package com.vchannel.cactus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,14 +12,27 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    String[] names = { "Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь", "Анна", "Денис", "Андрей" };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+
+        // находим список
+        ListView lvMain = (ListView) findViewById(R.id.issuesTable);
+
+        // создаем адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, names);
+
+        // присваиваем адаптер списку
+        lvMain.setAdapter(adapter);
     }
 
     /**
